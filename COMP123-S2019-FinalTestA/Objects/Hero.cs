72d07882f5bd26e4f0c2c2f4f7cf31670b5c9be5
301043771 <301043771@PR-E216-18>
2019-08-09 09:06:07 -0400

    Created Base Project

---
+++ COMP123-S2019-FinalTestA/Objects/Hero.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * STUDENT NAME:
+ * STUDENT NO.:
+ * DESCRIPTION: Tis is the hero data Container
+ */
+namespace COMP123_S2019_FinalTestA.Objects
+{
+    class Hero
+    {
+        // Identity
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        //Physcial Abilities 
+        public string Fighting { get; set; }
+        public string Strength { get; set; }
+        public string Agility { get; set; }
+        public string Endurance { get; set; }
+        
+        //Mental Abilities
+        public string Reason { get; set; }
+        public string Intuition { get; set; }
+        public string Pysche { get; set; }
+        public string Popularity { get; set; }
+
+        //Power List
+        List<Power> Powers;
+
+        //Cpnstructor
+
+        Hero()
+        {
+            //Instatiates an empty power List
+            Powers = new List<Power>();
+        }
+
+    }
+}
---
+++ COMP123-S2019-FinalTestA/Objects/Power.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * STUDENT NAME:
+ * STUDENT NO.:
+ * DESCRIPTION: Tis is the hero Power Class
+ */
+namespace COMP123_S2019_FinalTestA.Objects
+{
+    class Power
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+    }
+}
